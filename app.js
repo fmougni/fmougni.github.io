146ffece6d69abfd6a90a03b2b4e914c7340c669
fmougni <85298590+fmougni@users.noreply.github.com>
2023-03-14 22:52:29 +0100

    change name file glb

--- app.js
+++ app.js
@@ -117,7 +117,7 @@ class App{
 		// Load a glTF resource
 		loader.load(
 			// resource URL .
-			`coffee${id}.glb`,
+			`chair${id}.glb`,
 			// called when the resource is loaded
 			function ( gltf ) {
 
